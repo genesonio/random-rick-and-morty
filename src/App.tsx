468f99f254fc4a91c15f31c56a38e9561d24a26b
Genesio S Pacehco <genesio.s.p.28@gmail.com>
2022-05-27 14:09:05 -0300

    Adding if statment to only render the tag <img> if char?.image != null

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { useQuery } from 'react-query'
 import './App.css'
+import { randomInteger } from './services/randomInteger'
 
 interface Character {
   created: string
@@ -8,12 +9,22 @@ interface Character {
   gender: string
   id: number
   image: string
-  location: object
+  location: Location
   name: string
-  origin: object
+  origin: Origin
   species: string
   status: string
   type: string
+}
+
+interface Origin {
+  name: string
+  url: string
+}
+
+interface Location {
+  name: string
+  url: string
 }
 
 function getLastCharacterId() {
@@ -25,49 +36,73 @@ function getLastCharacterId() {
   })
 }
 
-async function getCharacterOrigin() {
-  const { data } = useQuery<number>('charOrigin', async () => {
-    const response = await axios.get('https://rickandmortyapi.com/api/character')
-    console.log(response.data)
-    return response.data
-  })
-}
+// async function getCharacterOrigin() {
+//   const { data } = useQuery<number>('charOrigin', async () => {
+//     const response = await axios.get('https://rickandmortyapi.com/api/character')
+//     console.log(response.data)
+//     return response.data
+//   })
+// }
 
-getCharacterOrigin()
+// getCharacterOrigin()
 
-function getRandomIntInclusive(min: number, max: number) {
-  min = Math.ceil(min)
-  max = Math.floor(max)
-  return Math.floor(Math.random() * (max - min + 1) + min)
-}
-
-const randomNumber = getRandomIntInclusive(1, 826)
+const randomNumber = randomInteger(1, 826)
 
 
 function App() {
-
-  const { data } = useQuery<Character>('char', async () => {
+  
+  const { data: char } = useQuery<Character>('char', async () => {
     const response = await axios.get(`https://rickandmortyapi.com/api/character/${randomNumber}`)
     console.log(response.data)
     return response.data
   })
 
-  const alt = 'Imagem do personagem: ' + data?.name
+
+  // if (char != undefined ) {
+  //   useQuery<Array<string>>('test', async () => {
+  //     const response = await axios.get(`${char?.episode[0]}`)
+  //     return response.data
+  //   })
+  // }
+
+  // const originUrl = char?.origin.url
+
+  // const { data: origin} = useQuery<Origin>('origin',async () => {
+  //   const response = await axios.get(`${originUrl}`)
+  //   console.log(response.data)
+  //   return response.data
+  // })
+
+  const alt = 'Imagem do personagem: ' + char?.name
 
   return (
-    <>
-      <div className="grid grid-cols-3 gap-1">
-        <div className="flex flex-col justify-center p-7">
+      <div className="flex justify-center bg-green-300 bg-contain w-screen h-screen">
+        { /* PERSONAGEM */ }
+        <div className="flex flex-col justify-center p-2">
+          {char?.image && 
           <img
-            src={data?.image}
+            src={char?.image}
             alt={alt}
-            className="rounded-full"
-          />
-          <h1 className='text-center font-serif text-4xl'>{data?.name}</h1>
+            className="rounded-full w-96 h-96 self-center"
+          />}
+
+          <h1 className='text-center font-serif text-4xl mt-7 mb-4'>{char?.name}</h1>
+
+          <div className="grid grid-cols-2 text-center">
+            <p>
+              Id: {char?.id} <br />
+              Status: {char?.status} <br />
+              Species: {char?.species} <br />
+              Gender: {char?.gender}
+            </p>
+            <p>
+              Origin: {char?.origin.name} <br />
+              Location: {char?.location.name} <br />
+              First episode: {}
+            </p>
+          </div>
         </div>
       </div>
-
-    </>
   )
 }
 
--- src/services/randomInteger.ts
+++ src/services/randomInteger.ts
@@ -1,3 +1,5 @@
-export async function randomInteger() {
-  return null
+export function randomInteger(min: number, max: number) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1) + min)
 }
